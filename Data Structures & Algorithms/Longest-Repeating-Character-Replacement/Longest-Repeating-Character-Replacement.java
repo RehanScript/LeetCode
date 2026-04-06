a8d7b719c433514205edb14934260d5745c87727
Md Rehan ALi <r.h.mediahouse.official@gmail.com>
2026-04-06 20:27:00 +0530

    optimal solution for the Longest Repeating Character Replacement

--- Data Structures & Algorithms/Longest-Repeating-Character-Replacement/Longest-Repeating-Character-Replacement.java
+++ Data Structures & Algorithms/Longest-Repeating-Character-Replacement/Longest-Repeating-Character-Replacement.java
@@ -1,1 +1,17 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        Map<Character, Integer> countFreq = new HashMap<>();
+        int maxFreq = 0,l=0, result = 0;
+        for(int i = 0; i<s.length(); i++) {
+            countFreq.put(s.charAt(i), countFreq.getOrDefault(s.charAt(i), 0) + 1);
+            maxFreq = Math.max(maxFreq, countFreq.get(s.charAt(i)));
 
+            while((i-l+1)-maxFreq > k){
+                countFreq.put(s.charAt(l), countFreq.get(s.charAt(l)) - 1 );
+                l++;
+            }
+            result = Math.max(result, i-l+1);
+        }
+        return result;
+    }
+}
